7ce3d689b4691dd6dc4fc26bb5dc3c395fa6ff87
Antonio Dolce <dolce.antonio@gmail.com>
2021-12-26 11:53:52 +0100

    added environment vars

--- src/main/java/com/myorg/SampleAppNewApp.java
+++ src/main/java/com/myorg/SampleAppNewApp.java
@@ -1,13 +1,37 @@
 package com.myorg;
 
+import java.util.Objects;
+
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
 
 public final class SampleAppNewApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new SampleAppNewStack(app, "SampleAppNewStack");
+        String environmentName = System.getenv("CDK_DEPLOY_STACK_NAME");
+        Objects.requireNonNull(environmentName, "environment variable 'CDK_DEPLOY_STACK_NAME' must not be null");
+        
+        Environment awsEnvironment = makeEnv(null, null);
+
+        new SampleAppNewStack(app, "SampleAppNewStack", StackProps.builder()
+                .stackName(environmentName + "-SampleApp")
+                .env(awsEnvironment)
+                .build());
 
         app.synth();
+    }
+
+    static Environment makeEnv(String account, String region) {
+        account = (account == null) ? System.getenv("CDK_DEPLOY_ACCOUNT") : account;
+        region = (region == null) ? System.getenv("CDK_DEPLOY_REGION") : region;
+        account = (account == null) ? System.getenv("CDK_DEFAULT_ACCOUNT") : account;
+        region = (region == null) ? System.getenv("CDK_DEFAULT_REGION") : region;
+
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
     }
 }
